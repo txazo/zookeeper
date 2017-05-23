653eca2c8e6fc59beeb6a675729e5d2edad6feb1
txazo <784990655@qq.com>
2017-05-23 19:03:39 +0800

    update

---
+++ src/test/java/test/server/QuorumPeerMainTest.java
@@ -0,0 +1,29 @@
+package test.server;
+
+import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+
+public class QuorumPeerMainTest {
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 5; i++) {
+            new Thread(new ZookeeperServer("/zoo" + i + ".cfg")).start();
+        }
+        System.in.read();
+    }
+
+    private static class ZookeeperServer implements Runnable {
+
+        private String config;
+
+        public ZookeeperServer(String config) {
+            this.config = config;
+        }
+
+        @Override
+        public void run() {
+            QuorumPeerMain.main(new String[]{ZookeeperServer.class.getResource(config).getPath()});
+        }
+
+    }
+
+}
